84e816205ac18f6cc8ddffe5f187e6fe1078bd11
msdeshmukh009 <deshmukhmahesh009@gmail.com>
2021-07-22 16:27:48 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+var bDate = document.querySelector("#bDate");
+var luckyNum = document.querySelector("#luckyNum");
+var checkBtn = document.getElementById("#checkBtn");
+
+function clicked(){
+    console.log("Clicked");
+}
+checkBtn.addEventListener("click",clicked());
+
+// window.onload= function (){
+//     //Your entire JS code here
+    
+//     }
