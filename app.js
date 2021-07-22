99b57bd51af84a46a59efb50062406a13b116c14
msdeshmukh009 <deshmukhmahesh009@gmail.com>
2021-07-22 17:31:58 +0530

    done with JS

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 var bDate = document.querySelector("#bDate");
 var luckyNum = document.querySelector("#luckyNum");
-var checkBtn = document.getElementById("#checkBtn");
+var button = document.querySelector("#btn1");
+var output = document.querySelector("#outputDiv")
 
 function clicked(){
-    console.log("Clicked");
+    var dateString = (bDate.value).split("-").join("");
+    var sum =0;
+    for(var i=0;i<dateString.length;i++){
+        sum = sum + Number(dateString[i]);
+    }
+   if(sum%Number(luckyNum.value)===0){
+    output.innerText ="yay"
+   }else{
+       output.innerText="nah"
+   }
 }
-checkBtn.addEventListener("click",clicked());
-
-// window.onload= function (){
-//     //Your entire JS code here
-    
-//     }
+button.addEventListener("click",clicked);
