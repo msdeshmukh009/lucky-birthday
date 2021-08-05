3c067f3623d5acfc7933562936097e4f6cbc6bc5
msdeshmukh009 <deshmukhmahesh009@gmail.com>
2021-08-06 02:11:06 +0530

    updated: CSS JS HTML

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ var bDate = document.querySelector("#bDate");
 var luckyNum = document.querySelector("#luckyNum");
 var button = document.querySelector("#btn1");
 var output = document.querySelector("#outputDiv")
+var closeBtn = document.querySelector("#close-notice")
+var noticeDiv = document.querySelector(".notice")
 
+const outputPass=`<div class="label">Hurray!!You are a lucky person!</div><img width="100%" height="200px" src="undraw_awesome_rlvy.svg" alt="image">`
+const outputFail=`<div class="label">Oops!!Your birthday is not a lucky number!</div><img width="100%" height="200px" src="undraw_Raining_re_4b55.svg" alt="image">`
 function clicked(){
     var dateString = (bDate.value).split("-").join("");
     var sum =0;
@@ -10,9 +14,13 @@ function clicked(){
         sum = sum + Number(dateString[i]);
     }
    if(sum%Number(luckyNum.value)===0){
-    output.innerText ="yay"
+    output.innerHTML =outputPass;
    }else{
-       output.innerText="nah"
+       output.innerHTML=outputFail;
    }
 }
+function closeNotice(){
+    noticeDiv.style.display="none"
+}
+closeBtn.addEventListener("click",closeNotice)
 button.addEventListener("click",clicked);
