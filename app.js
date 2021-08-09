240d61d6333c6bb17054802c4e3611843cd30afd
msdeshmukh009 <deshmukhmahesh009@gmail.com>
2021-08-10 02:21:21 +0530

    updated: app.js & index.html & SVGs

--- app.js
+++ app.js
@@ -7,32 +7,22 @@ var noticeDiv = document.querySelector(".notice")
 var form = document.querySelector("#form")
 
 
-const outputPass=`<div class="label">Hurray!!You are a lucky person!</div><img width="100%" height="200px" src="undraw_awesome_rlvy.svg" alt="image">`
-const outputFail=`<div class="label">Ohh!!Your birthday is not that lucky!</div><img width="100%" height="200px" src="undraw_Raining_re_4b55.svg" alt="image">`
+const outputPass=`<div class="label">Hurray!!You are a lucky person!</div><img width="100%" height="200px"  src="undraw_awesome_rlvy.svg" alt="image">`
+const outputFail=`<div class="label">Ohh!!Your birthday is not that lucky!</div><img width="100%" height="200px"  src="undraw_Raining_re_4b55.svg" alt="image">`
 
-// function clicked(){
-//     preventDefault();
-//     var dateString = (bDate.value).split("-").join("");
-//     var sum =0;
-//     for(var i=0;i<dateString.length;i++){
-//         sum = sum + Number(dateString[i]);
-//     }     
-//             if(sum%Number(luckyNum.value)===0){
-//                     output.innerHTML =outputPass;
-//             }else{
-//                  output.innerHTML=outputFail;
-//                    }     
-//         }
 
-form.addEventListener('submit', (e)=>{
+
+form.addEventListener("submit", (e)=>{
     e.preventDefault();
-    var dateString = ((bDate.value).split('-')).join("");
-    var sum=0;
-    for(var i=0;i<dateString.length;i++){
-     sum = sum + Number(dateString[i]);  
+    var dateString = (bDate.value).split("-").join("");
+    let sum =0;
+   
+    for(var i=0;i< dateString.length;i++){
+        sum = sum + Number(dateString[i]); 
     }
     
-    if(sum%luckyNum ===0){
+    const luckyNo= Number(luckyNum.value);
+    if(sum%luckyNo === 0){
         outputDiv.innerHTML = outputPass;
     }
     else{
@@ -40,8 +30,7 @@ form.addEventListener('submit', (e)=>{
     }
 })
 
-function closeNotice(){
+closeBtn.addEventListener("click",()=>{
     noticeDiv.style.display="none"
-}
-closeBtn.addEventListener("click",closeNotice)
+})
 
