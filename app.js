dadb517be17e14bcac61959ddbef57948afda0e7
msdeshmukh009 <deshmukhmahesh009@gmail.com>
2021-08-16 01:28:37 +0530

    fixed: app.js

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ var noticeDiv = document.querySelector(".notice")
 var form = document.querySelector("#form")
 
 
-const outputPass=`<div class="label">Hurray!!You are a lucky person!</div><img width="100%" height="200px"  src="undraw_awesome_rlvy.svg" alt="image">`
-const outputFail=`<div class="label">Ohh!!Your birthday is not that lucky!</div><img width="100%" height="200px"  src="undraw_Raining_re_4b55.svg" alt="image">`
+const outputPass='<div>Hurray!!You are a lucky person!</div><img width="100%" height="200px"  src="undraw_awesome_rlvy.svg" alt="image">'
+const outputFail='<div>Ohh!!Your birthday is not that lucky!</div><img width="100%" height="200px"  src="undraw_Raining_re_4b55.svg" alt="image">'
 
 
 
